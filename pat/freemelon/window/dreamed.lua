3d41dbc626eb2f29e32cf6a86790a521d276d576
patman <27874300+patmanf@users.noreply.github.com>
2023-07-03 17:47:00 -0500

    Congratulations. You've found the folder containing my anti melon system. If you're here to admire my handiwork, then go ahead and admire. But if you're here to try and disable it, then you might as well give up. I have planned for every form of attack, and my system and will continue to fulfill its purpose no matter how long and hard you work to turn it off. If you're still bent on proving me wrong, then go right ahead. You'll soon see how hopeless it is.

--- pat/freemelon/window/dreamed.lua
+++ pat/freemelon/window/dreamed.lua
@@ -1,3 +1,11 @@
+--[[
+
+Congratulations. You've found the folder containing my anti melon system. If you're here to admire my handiwork, then go ahead and admire. But if you're here to try and 
+disable it, then you might as well give up. I have planned for every form of attack, and my system and will continue to fulfill its purpose no matter how long and hard you 
+work to turn it off. If you're still bent on proving me wrong, then go right ahead. You'll soon see how hopeless it is.
+
+]]
+
 local t = 0
 local h = math.random(1, 5)
 function update(dt)
